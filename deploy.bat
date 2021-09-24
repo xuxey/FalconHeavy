7b5a7d835ffb1d2c9a766e4ddc158ed42d45fe1c
Soham Kulkarni <sohamkrox@gmail.com>
2021-09-24 13:41:45 -0500

    Delete deploy.bat

--- deploy.bat
+++
@@ -1,8 +0,0 @@
-call echo running Falcon build routine
-call del "D:\Java Workspace\FalconHeavy\build\libs\falcon.jar"
-call gradle build
-call gradle jar
-call rename "D:\Java Workspace\FalconHeavy\build\libs\FalconHeavy-1.0-SNAPSHOT-all.jar" "falcon.jar"
-call echo starting SCP upload
-call scp "D:\Java Workspace\FalconHeavy\build\libs\falcon.jar" xuxe@40.77.111.129:/home/xuxe/falcon
-echo Finished Falcon build routine
